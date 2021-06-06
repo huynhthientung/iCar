5244045e5f43bae6fa8c9c1b68c685cc27bb1c99
Tung Huynh <huynhthientung.dev@gmail.com>
2021-06-06 17:36:07 +0700

    working on sign in and sign up activity

--- app/src/main/java/com/example/icar/MainActivity.java
+++ app/src/main/java/com/example/icar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.icar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,12 +8,30 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText edtEmail, edtPassword;
+    private CheckBox checkBox;
+    private FloatingActionButton btnEnter;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -21,9 +40,53 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        edtEmail = findViewById(R.id.editTextEmail);
+        edtPassword = findViewById(R.id.editTextPassword);
+        btnEnter = findViewById(R.id.floatingButtonEnter);
+        checkBox = findViewById(R.id.checkboxShowPassword);
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSignIn();
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                }
+            }
+        });
+    }
+
+    private void onSignIn() {
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Successfully sign-in", Toast.LENGTH_SHORT).show();
+                    // TODO: move on to dashboard
+                } else {
+                    Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void OpenSignupPage(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
+
+    public void OpenForgotPasswordPage(View view) {
+        // TODO: handle later
     }
 }
--- app/src/main/java/com/example/icar/SignUpActivity.java
+++ app/src/main/java/com/example/icar/SignUpActivity.java
@@ -1,14 +1,34 @@
 package com.example.icar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText edtName, edtEmail, edtPassword;
+    private FloatingActionButton btnEnter;
+    private CheckBox checkBox;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,5 +37,57 @@ public class SignUpActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_sign_up);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        edtName = findViewById(R.id.editTextName);
+        edtEmail = findViewById(R.id.editTextEmail);
+        edtPassword = findViewById(R.id.editTextPassword);
+        btnEnter = findViewById(R.id.floatingButtonEnter);
+        checkBox = findViewById(R.id.checkboxShowPassword);
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCreateNewUser();
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                }
+            }
+        });
+
+
     }
+
+    private void onCreateNewUser() {
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // TODO: move on dashboard
+                            Toast.makeText(SignUpActivity.this, "Successfully sign-up", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "" + task.getException().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void OpenSigninPage(View view) {
+        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+    }
+
 }
