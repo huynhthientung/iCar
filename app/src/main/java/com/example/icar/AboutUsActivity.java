1e31f4119cc80c5bfa863b4f23d091e2ebfe5f4a
Tung Huynh <huynhthientung.dev@gmail.com>
2021-06-22 18:13:40 +0700

    handle license

---
+++ app/src/main/java/com/example/icar/AboutUsActivity.java
@@ -0,0 +1,30 @@
+package com.example.icar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
