84c469345581300a972aabb8fb196744309ae34d
Tung Huynh <huynhthientung.dev@gmail.com>
2021-07-01 22:28:16 +0700

    add booking activity

--- app/src/main/java/com/example/icar/activity/BookingActivity.java
+++ app/src/main/java/com/example/icar/activity/BookingActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.icar.R;
 
 import java.util.Objects;
 
 public class BookingActivity extends AppCompatActivity {
+    private Spinner spinnerSource, spinnerDestination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
         Objects.requireNonNull(this.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        initViews();
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(this, R.array.provinces, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSource.setAdapter(adapter);
+
+    }
+
+    private void initViews() {
+        spinnerSource = findViewById(R.id.spinner_destination);
     }
 
     @Override
